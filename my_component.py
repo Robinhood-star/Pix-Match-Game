b56a87a16171ed2a6208d0096b248ab478623496
Robinhood-star <manishq766@gmail.com>
2025-06-08 12:19:27 +0530

    Update my_component.py

--- my_component.py
+++ my_component.py
@@ -3,4 +3,6 @@ import streamlit as st
 def pix_match(difficulty):
     st.markdown(f"### You selected: {difficulty}")
     st.markdown("#### 🎮 Game will appear below:")
-    st.components.v1.iframe("https://keen-mousse-d356d0.netlify.app", height=600)
+
+    # Increase iframe height to fully display the game
+    st.components.v1.iframe("https://keen-mousse-d356d0.netlify.app", height=850)
