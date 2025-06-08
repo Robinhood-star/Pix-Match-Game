60cc88f5ddfde97cc96870acc037a1103cceac0d
Robinhood-star <manishq766@gmail.com>
2025-06-08 19:52:10 +0530

    Update my_component.py

--- my_component.py
+++ my_component.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
 def pix_match(difficulty):
-    # Dynamically increase the height only after game starts
-    iframe_height = 1150  # Adjust this if still cut off
-
-    # Embed full game view
-    st.components.v1.iframe("https://keen-mousse-d356d0.netlify.app", height=iframe_height, scrolling=False)
+    # Embed game fullscreen with no scroll
+    st.components.v1.iframe("https://keen-mousse-d356d0.netlify.app", height=1400, scrolling=False)
