dacf535e33c78231a7ffada6c22f9d69bf5c45c6
Robinhood-star <manishq766@gmail.com>
2025-06-22 10:36:02 +0530

    Update my_component.py

--- my_component.py
+++ my_component.py
@@ -10,7 +10,7 @@ def pix_match(difficulty):
         <style>
         .responsive-iframe {
             position: relative;
-            padding-bottom: 56.25%; /* 16:9 ratio */
+            padding-bottom: 80vh; /* vh = viewport height for better mobile view */
             height: 0;
             overflow: hidden;
             max-width: 100%;
@@ -22,9 +22,11 @@ def pix_match(difficulty):
             top: 0;
             left: 0;
             width: 100%;
-            height: 100%;   
+            height: 100%;
+            border: none;
         }
         </style>
+
         <div class="responsive-iframe">
             <iframe src="https://keen-mousse-d356d0.netlify.app/" frameborder="0" allowfullscreen></iframe>
         </div>
